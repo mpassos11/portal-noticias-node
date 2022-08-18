bb5fd13ebd308c94d04c568de486848a98c79e58
Matheus Passos <matheus.passos@multidadosti.com.br>
2022-08-18 12:37:27 -0300

    criando rota e preparando arquivos

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, '/public')));
 app.set('views', path.join(__dirname, '/views'));
 
+app.get('/', function (req, res) {
+    if (req.query.busca == null) {
+        res.send('home');
+    } else {
+        res.send('Você buscou por: ' + req.query.busca);
+    }
+});
+
+app.get('/:slug', function(req, res) {
+    res.send(req.params.slug);
+});
+
 app.listen(5000, function() {
     console.log('Server running at http://localhost:5000');
 });
