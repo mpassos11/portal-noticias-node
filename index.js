3827320a9505384b3f674dadc82957d7a4ee5789
Matheus Passos <matheus.passos@multidadosti.com.br>
2022-08-18 16:48:59 -0300

    lista ultima noticia como principal

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const path = require('path');
 
 const app = express();
 
-mongoose.connect(`mongodb+srv://root:${process.env.PASSWORD_MONGODB}@cluster0.gvzza.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }).then(function() {
+const Posts = require('./Posts.js');
+
+mongoose.connect(`mongodb+srv://root:${process.env.PASSWORD_MONGODB}@cluster0.gvzza.mongodb.net/dankicode?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }).then(function() {
     console.log('Connected to MongoDB...');
 }).catch(function(err) {
     console.error(err.message);
@@ -23,7 +25,13 @@ app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', function (req, res) {
     if (req.query.busca == null) {
-        res.render('home', {});
+        Posts.find({}).sort({ '_id': -1 }).exec(function (err, posts) {
+            posts = posts.map(function(val) {
+                val.descricaoCurta = val.conteudo.substr(0, 100);
+                return val;
+            })
+            res.render('home', { posts: posts });
+        });
     } else {
         res.render('busca', {});
     }
